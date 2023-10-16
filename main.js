2dc7e055414fbf93ba84243ff5330759f713c987
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-10-16 11:39:28 +0800

    feat: operation for all 4 calculations

---
+++ main.js
@@ -0,0 +1,24 @@
+function add(){
+    let secondNumber;
+    let firstNumber;
+    let operator;
+    operator = firstNumber + secondNumber; 
+}
+function subtract(){
+    let secondNumber;
+    let firstNumber;
+    let operator;
+    operator = firstNumber - secondNumber; 
+}
+function multiply(){
+    let secondNumber;
+    let firstNumber;
+    let operator;
+    operator = firstNumber * secondNumber; 
+}
+function divide(){
+    let secondNumber;
+    let firstNumber;
+    let operator;
+    operator = firstNumber / secondNumber; 
+}
