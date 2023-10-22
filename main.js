db11ce697781dd0041673d66796f100cdacbe574
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-10-22 13:07:04 +0800

    fix&feat: clean code and add comments

--- main.js
+++ main.js
@@ -6,27 +6,31 @@ let num2 = ""
 let isFirstNum = true
 
 // calculation
-function add(num1, num2){
+function add(num1, num2) {
     return num1 + num2
 }
-function subtract(num1, num2){
+function subtract(num1, num2) {
     return num1 - num2
 }
-function multiply(num1, num2){
+function multiply(num1, num2) {
     return num1 * num2
 }
-function divide(num1, num2){
+function divide(num1, num2) {
     return num1 / num2
 }
 // operation
-function operate(operator, num1, num2){
-    
-}
-function populateDisplay(){
-    buttons.forEach((button) =>{
-        button.addEventListener("click", () =>{
-            let btnId = button.id
+function operate(operator, num1, num2) {
 
+}
+
+// display on HTML
+function populateDisplay() {
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            // assign button.id to btnId
+            let btnId = button.id
+            
+            // condition where to store number
             if (!isFirstNum) {
                 num2 += btnId
                 console.log(` num2 ${num2} `)
@@ -36,12 +40,7 @@ function populateDisplay(){
                 console.log(` num2 ${num2} `)
                 display.innerText = num1;
             }
+            
         })
     })
 }
-buttons.forEach((button) => {
-    button.addEventListener("click", ()=>{
-        let btn = button.id
-        operate(btn)
-    })
-})
