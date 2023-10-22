afaa77281eba61e3afcce8ad76d447a8c236b195
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-10-22 13:40:03 +0800

    add logic inside switch(operator)

--- main.js
+++ main.js
@@ -34,7 +34,22 @@ function divide(num1, num2) {
 // operation
 function operate(operator, num1, num2) {
     switch(operator){
-        
+        case "+":
+            add(num1, num2)
+            display.innerText = total
+            break;
+        case "-":
+            subtract(num1, num2)
+            display.innerText = total
+            break;
+        case "*":
+            multiply(num1, num2)
+            display.innerText = total
+            break;
+        case "/":
+            divide(num1, num2)
+            display.innerText = total
+            break;
     }
 }
 
