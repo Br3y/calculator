f51870eccd0494029202ca0899621dea031208cd
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-10-22 13:25:37 +0800

    change calculation logic

--- main.js
+++ main.js
@@ -16,16 +16,20 @@ function clr() {
 
 // calculation
 function add(num1, num2) {
-    return num1 + num2
+    total = num1 + num2
+    return total
 }
 function subtract(num1, num2) {
-    return num1 - num2
+    total = num1 - num2
+    return total
 }
 function multiply(num1, num2) {
-    return num1 * num2
+    total = num1 * num2
+    return total
 }
 function divide(num1, num2) {
-    return num1 / num2
+    total = num1 / num2
+    return total
 }
 // operation
 function operate(operator, num1, num2) {
